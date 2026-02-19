1cab48cc2dbd6ec7b0bf1c6620dd70343831b0de
diskfoexe <diskfo.exe@gmail.com>
2026-02-19 19:04:37 +0200

    Update __init__.py

--- custom_nodes/DiskfoNodePack/__init__.py
+++ custom_nodes/DiskfoNodePack/__init__.py
@@ -1,12 +1,17 @@
 from .lora_loader_node import NODE_CLASS_MAPPINGS as LoraMappings
 from .lora_loader_node import NODE_DISPLAY_NAME_MAPPINGS as LoraDisplayMappings
 
+from .timer_nodes import NODE_CLASS_MAPPINGS as TimerMappings
+from .timer_nodes import NODE_DISPLAY_NAME_MAPPINGS as TimerDisplayMappings
+
 NODE_CLASS_MAPPINGS = {
     **LoraMappings,
+    **TimerMappings,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     **LoraDisplayMappings,
+    **TimerDisplayMappings,
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
